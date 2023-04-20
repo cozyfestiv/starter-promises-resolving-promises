70e696e4b565ffdbee7c0fc442d546bd261b8f3e
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2023-04-20 10:08:10 -0400

    Update main.js
    updated body to include the tellPromise

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-const { welcome, goodbye, tell } = require("../utils/fortune-teller");
+const { welcome, goodbye, tell } = require('../utils/fortune-teller')
 
-const promise = welcome();
+const promise = welcome()
 
-console.log(promise);
+console.log(promise)
+
+const question = 'Will the weather be nice today?'
+
+const tellPromise = tell(question)
+
+tellPromise
+  .then(fortune => {
+    console.log(question)
+
+    console.log(fortune)
+  })
+  .catch(console.error)
+
+welcome().then(console.log).then(goodbye).then(console.log).catch(console.error)
